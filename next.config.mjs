ce8ace9c20a81ff27c4d0056013b3dd53745d699
[Puneet] <puneetchandna21@gmail.com>
2024-08-26 05:46:22 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    trailingSlash: true, 
+    images: {
+      unoptimized: true, 
+    },
+    basePath: '/portfolio-frontend', // Replace with your GitHub repository name
+  };
+  
+  export default nextConfig;
+  
